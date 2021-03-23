06c25a15a6641d208a1aaa4bfac2c8e7e671fa10
Sidharth Soman <siddu.soman65@gmail.com>
2021-03-23 16:48:52 +0530

    did stuff practice stuff

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+console.log('Testing');
 document.querySelector('#get').addEventListener('click', run);
 
 async function run(){
     let name = document.querySelector('input').value;
-    let res = await fetch(`/api?student=${name}&age`);
+    let res = await fetch(`/api?name=${name}&age=12&gender='male`);
     let data = await res.json();
-    document.querySelector('#name').innerText = data.name;
+    console.log(data);
+    document.querySelector('#theName').innerText = data.name;
 }
--- server.js
+++ server.js
@@ -2,11 +2,44 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     const page = url.parse(req.url).pathname;
     console.log(page);
-    res.end();
+    if(page == '/'){
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(page == '/css/style.css'){
+        fs.readFile('css/style.css', (err, data) => {
+            res.write(data);
+            res.end();
+        });
+    }
+    else if(page == '/js/index.js'){
+        fs.readFile('js/index.js', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.write(data);
+            res.end();
+        });
+    }
+    else if(page == '/api'){
+        const params = querystring.parse(url.parse(req.url).query);
+        const obj = {name: params.name};
+        res.end(JSON.stringify(obj));
+    }
+    else{
+        figlet('ERROR 404: PAGE NOT FOUND', (err, data) => {
+            if(err) throw err;
+            else{
+                res.write(data);
+                res.end();
+            }
+        })
+    }
 });
 
 server.listen(5000); //listen on port 5000
